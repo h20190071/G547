12410457bb782491df2d564a31f30ef3b731d4d6
h20190071 <60569010+h20190071@users.noreply.github.com>
2020-03-30 22:59:21 +0530

    Delete .main.ko.cmd

--- assignment2/.main.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/akanksha/device_driver/assignment2/main.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/akanksha/device_driver/assignment2/main.ko /home/akanksha/device_driver/assignment2/main.o /home/akanksha/device_driver/assignment2/main.mod.o ;  true
