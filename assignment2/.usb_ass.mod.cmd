a47e97a63c271306abb66e2bafa25e6ba252fcf4
h20190071 <60569010+h20190071@users.noreply.github.com>
2020-03-30 23:00:26 +0530

    Delete .usb_ass.mod.cmd

--- assignment2/.usb_ass.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/akanksha/device_driver/assignment2/usb_ass.mod := { echo  /home/akanksha/device_driver/assignment2/usb_ass.o; echo; } > /home/akanksha/device_driver/assignment2/usb_ass.mod
